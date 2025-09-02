2bd1700bc02d0a7f5ddb2c1c88f32b632dad4170
MBearo <wuweiqi@MacBook-Pro-3.local>
2025-09-02 11:47:25 +0800

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Task1 from './pages/test1'
-import Task2 from './pages/test2'
 
 
 function App() {
@@ -8,8 +7,6 @@ function App() {
     <>
       <h2>任务管理器</h2>
       <Task1 />
-      <h2>生物群系生成系统</h2>
-      <Task2 />
     </>
   )
 }
